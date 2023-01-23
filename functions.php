55679c28005ceb715dd89f6cfce621fe64d0fe30
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-01-23 13:58:39 -0300

    add bootstrap in theme

--- functions.php
+++ functions.php
@@ -16,11 +16,21 @@
 
 function pedagogia_concursos_enqueue_scripts()
 {
+  // Resgister Styles
   wp_register_style('pedagogia-concursos', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
-  wp_register_script('pedagogia-concursos', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
+  wp_register_style('bootstrap', get_template_directory_uri() . '/assets/src/css/bootstrap.min.css', [], false, 'all');
 
-  wp_enqueue_style( 'pedagogia-concursos' );
-  wp_enqueue_script( 'pedagogia-concursos' );
+  // Register Scripts
+  wp_register_script('pedagogia-concursos', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
+  wp_register_script('bootstrap', get_template_directory_uri() . '/assets/src/js/bootstrap.bundle.min.js', ['jquery'], false, true);
+
+  // Enqueue Styles
+  wp_enqueue_style('pedagogia-concursos');
+  wp_enqueue_style('bootstrap');
+
+  // Enqueue Cripts
+  wp_enqueue_script('pedagogia-concursos');
+  wp_enqueue_script('bootstrap');
 }
 
 add_action('wp_enqueue_scripts', 'pedagogia_concursos_enqueue_scripts');
