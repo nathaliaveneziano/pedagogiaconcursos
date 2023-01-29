879565f0325d5d86c59ced88dfed2b908b681067
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-01-29 16:32:24 -0300

    add custom logo image

--- inc/classes/class-pedagogia-concursos-theme.php
+++ inc/classes/class-pedagogia-concursos-theme.php
@@ -32,5 +32,13 @@ class PEDAGOGIA_CONCURSOS_THEME
   public function setup_theme()
   {
     add_theme_support('title-tag');
+
+    add_theme_support('custom-logo', [
+      'header-text' => ['site-title', 'site-description'],
+      'height' => 100,
+      'width' => 400,
+      'flex-height' => true,
+      'flex-width' => true,
+    ]);
   }
 }
