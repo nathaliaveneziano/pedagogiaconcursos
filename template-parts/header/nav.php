ab23035c5198ffb436521f7545842afc1bdbf606
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-01-30 19:12:16 -0300

    navigation dynamic

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -5,6 +5,11 @@
  *
  * @package Pedagogia Concursos
  */
+
+$menu_class = \PEDAGOGIA_CONCURSOS\Inc\Menus::get_instance();
+$header_menu_id = $menu_class->get_menu_id('pc-header-menu');
+
+$header_menus = wp_get_nav_menu_items($header_menu_id);
 ?>
 
 
@@ -19,30 +24,58 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
+      <?php
+      if (!empty($header_menus) && is_array($header_menus)) {
+      ?>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <?php
+          foreach ($header_menus as $menu_item) {
+            if (!$menu_item->menu_item_parent) {
+
+              $child_menu_items = $menu_class->get_child_menu_items($header_menus, $menu_item->ID);
+              $has_children = !empty($child_menu_items) && is_array($child_menu_items);
+
+              if (!$has_children) {
+          ?>
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="<?= esc_url($menu_item->url); ?>">
+                    <?= esc_html($menu_item->title); ?>
+                  </a>
+                </li>
+              <?php
+              } else {
+              ?>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <?= esc_html($menu_item->title); ?>
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <?php
+                    foreach ($child_menu_items as $child_menu_item) {
+                    ?>
+                      <li><a class="dropdown-item" href="<?= esc_url($child_menu_item->url); ?>">
+                          <?= esc_html($child_menu_item->title); ?>
+                        </a></li>
+                    <?php
+                    }
+                    ?>
+                    <li>
+                      <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="<?= esc_url($menu_item->url); ?>">Todos os <?= esc_html($menu_item->title); ?></a></li>
+                  </ul>
+                </li>
+              <?php
+              }
+              ?>
+          <?php
+            }
+          }
+          ?>
+        </ul>
+      <?php
+      }
+      ?>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
